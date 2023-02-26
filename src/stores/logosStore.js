a91859a6af539bdd9d5d9ea52f4b82771d3d1d13
Askhat <73778760+verozroke@users.noreply.github.com>
2023-02-27 03:59:20 +0600

    wtf

--- src/stores/logosStore.js
+++ src/stores/logosStore.js
@@ -4,28 +4,28 @@ const useLogosStore = defineStore('logosStore', {
     state: () => ({
         logos: {
             babyCareLogos: [
-                './public/logos/babyCare_1.png',
+                './logos/babyCare_1.png',
             ],
             dishCareLogos: [
-                './public/logos/dishCare_1.png',
-                './public/logos/dishCare_2.png',
-                './public/logos/dishCare_3.png',
+                './logos/dishCare_1.png',
+                './logos/dishCare_2.png',
+                './logos/dishCare_3.png',
             ],
             femCareLogos: [
-                './public/logos/femCare_1.png',
-                './public/logos/femCare_2.jpg',
-                './public/logos/femCare_3.jpg',
+                './logos/femCare_1.png',
+                './logos/femCare_2.jpg',
+                './logos/femCare_3.jpg',
             ],
             hairCareLogos: [
-                './public/logos/hairCare_1.png',
-                './public/logos/hairCare_2.jpg',
-                './public/logos/hairCare_3.png',
-                './public/logos/hairCare_4.png',
+                './logos/hairCare_1.png',
+                './logos/hairCare_2.jpg',
+                './logos/hairCare_3.png',
+                './logos/hairCare_4.png',
             ],
             laundryCareLogos: [
-                './public/logos/laundryCare_1.png',
-                './public/logos/laundryCare_2.png',
-                './public/logos/laundryCare_3.jpg',
+                './logos/laundryCare_1.png',
+                './logos/laundryCare_2.png',
+                './logos/laundryCare_3.jpg',
             ],
         }
     })
